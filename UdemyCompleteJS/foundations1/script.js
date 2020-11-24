de54e5a25e7cd6b9defa74c5ba9110129b04bc99
fredwbaker <fredwbaker@gmail.com>
2020-11-24 07:02:13 -0600

    First course coding challenge complete!

--- UdemyCompleteJS/foundations1/script.js
+++ UdemyCompleteJS/foundations1/script.js
@@ -150,3 +150,38 @@ console.log(ageFred, ageGeorge, averageAge);
 
 //test commit for new repo
 
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let markBMI = (massMark / (heightMark **2));
+
+let johnBMI = (massJohn / (heightJohn **2));
+
+
+console.log(markBMI, johnBMI);
+
+
+
+let markHigherBMI = (markBMI>johnBMI);
+
+console.log(markHigherBMI);
+
+
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn = 1.76;
+
+markBMI = (massMark / (heightMark **2));
+
+johnBMI = (massJohn / (heightJohn **2));
+
+
+console.log(markBMI, johnBMI);
+
+
+markHigherBMI = (markBMI>johnBMI);
+
+console.log(markHigherBMI);
